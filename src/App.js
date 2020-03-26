829d4f0ff34d743cd1a3e6676587f66d4c575407
mark <mark@sourcepoint.com>
2020-03-26 10:28:02 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <View />
+      <View2/>
     </div>
   );
 }
+
+function View() {
+  const [inputs, setInputs] = useState([])
+  const prevLengthRef = useRef(inputs.length)
+  const containerRef = useRef(null)
+
+  const prevLength = prevLengthRef.current
+  useEffect(() => {
+    if (prevLength < inputs.length) {
+      const container = containerRef.current
+      container.scrollTop = container.scrollHeight
+    }
+    prevLengthRef.current = inputs.length
+  }, [inputs.length])
+
+  const addInput = () => {
+    setInputs(inputs => [
+      ...inputs,
+      { label: `Input number ${inputs.length}` }
+    ])
+  }
+
+  return (
+    <div className="view-container">
+      <div ref={containerRef} className="inputs-container">
+        {inputs.map(({ label }) => (
+          <div key={label} className="input-container">
+            <label>{label}</label>
+            <input />
+          </div>
+        ))}
+      </div>
+      <div className="add-input-btn" onClick={addInput}>
+        Add Input
+      </div>
+    </div>
+  )
+}
+
+function View2() {
+  const [dropdowns, setDropdowns] = useState([])
+  const containerRef = useAutoScroll(dropdowns.length)
+
+  const addDropdown = () => {
+    setDropdowns(dropdowns => [
+      ...dropdowns,
+      { label: `Dropdown number ${dropdowns.length}` }
+    ])
+  }
+
+  return (
+    <div className="view-container">
+      <div ref={containerRef} className="inputs-container">
+        {dropdowns.map(({ label }) => (
+          <div key={label} className="input-container">
+            <label>{label}</label>
+            <select>
+              <option>opt 1</option>
+              <option>opt 2</option>
+            </select>
+          </div>
+        ))}
+      </div>
+      <div className="add-input-btn" onClick={addDropdown}>
+        Add Input
+      </div>
+    </div>
+  )
+}
+
+function useAutoScroll(length) {
+  const element = useRef(null);
+  const prevLengthRef = useRef(length);
+  const prevLength = prevLengthRef.current;
+  useEffect(() => {
+    prevLengthRef.current = length;
+    if (prevLength < length && element.current) {
+      element.current.scrollTop = element.current.scrollHeight;
+    };
+  }, [length]);
+
+  return element;
+}
+
 
 export default App;
